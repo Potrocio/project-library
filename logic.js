ebb2a63eab87d1c654fe1e84d4a6f2739f367c95
Potrocio <john.feliciano4044@yahoo.com>
2022-11-02 17:39:22 -0400

    finish basic template with css and html, test some js code

---
+++ logic.js
@@ -0,0 +1,9 @@
+let myLibrary = [];
+
+function Book(title,author,read) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+};
+
+const htmlBooks = document.querySelector('.main-column');
